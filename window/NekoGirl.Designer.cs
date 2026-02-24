9b87372c262635292a112b7792d90c0b20945f6a
YUZU-384 <167997845+YUZU-384@users.noreply.github.com>
2026-02-24 22:34:42 +0800

    添加项目文件。

---
+++ window/NekoGirl.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NekoGirl
+{
+    partial class NekoGirl
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NekoGirl));
+            pictureBox = new PictureBox();
+            button_上 = new Button();
+            button_下 = new Button();
+            button_存 = new Button();
+            button_看 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox.BackColor = SystemColors.Control;
+            pictureBox.BackgroundImageLayout = ImageLayout.None;
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.Margin = new Padding(0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(384, 520);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // button_上
+            // 
+            button_上.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_上.Enabled = false;
+            button_上.Location = new Point(5, 525);
+            button_上.Name = "button_上";
+            button_上.Size = new Size(55, 30);
+            button_上.TabIndex = 0;
+            button_上.TabStop = false;
+            button_上.Text = "上一只";
+            button_上.UseVisualStyleBackColor = true;
+            button_上.Click += Button_上_Click;
+            // 
+            // button_下
+            // 
+            button_下.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_下.Location = new Point(66, 525);
+            button_下.Name = "button_下";
+            button_下.Size = new Size(55, 30);
+            button_下.TabIndex = 0;
+            button_下.TabStop = false;
+            button_下.Text = "下一只";
+            button_下.UseVisualStyleBackColor = true;
+            button_下.Click += Button_下_Click;
+            // 
+            // button_存
+            // 
+            button_存.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button_存.Location = new Point(219, 525);
+            button_存.Name = "button_存";
+            button_存.Size = new Size(65, 30);
+            button_存.TabIndex = 0;
+            button_存.TabStop = false;
+            button_存.Text = "保存图片";
+            button_存.UseVisualStyleBackColor = true;
+            button_存.Click += Button_存_Click;
+            // 
+            // button_看
+            // 
+            button_看.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button_看.Location = new Point(289, 525);
+            button_看.Name = "button_看";
+            button_看.Size = new Size(90, 30);
+            button_看.TabIndex = 0;
+            button_看.TabStop = false;
+            button_看.Text = "打开保存目录";
+            button_看.UseVisualStyleBackColor = true;
+            button_看.Click += Button_看_Click;
+            // 
+            // NekoGirl
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(384, 561);
+            Controls.Add(button_看);
+            Controls.Add(button_存);
+            Controls.Add(button_下);
+            Controls.Add(button_上);
+            Controls.Add(pictureBox);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimumSize = new Size(400, 600);
+            Name = "NekoGirl";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "NekoGirl-猫娘下载器，随机获取猫娘";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        public PictureBox pictureBox;
+        public Button button_上;
+        public Button button_下;
+        private Button button_存;
+        private Button button_看;
+    }
+}
---
+++ window/NekoGirl.cs
@@ -0,0 +1,104 @@
+﻿using NekoGirl.web;
+using System.Diagnostics;
+using System.Security.Policy;
+
+namespace NekoGirl
+{
+    public partial class NekoGirl : Form
+    {
+        //启动事件
+
+        private readonly string SaveDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "neko");
+        private readonly GetImage img = new ();
+        private int currentIndex = -1;//url数组从0开始
+
+        public NekoGirl()
+        {
+            InitializeComponent();
+
+            Debug.WriteLine("保存目录: " + SaveDirectory);
+            Directory.CreateDirectory(SaveDirectory);
+
+            Load += NekoGirl_Load;//启动自动加载图片
+        }
+
+        private async void NekoGirl_Load(object? sender, EventArgs e)
+        {
+            await img.GetNextSetAsync();
+            //await LoadImageAsync(currentIndex);
+        }
+
+
+        //按钮事件
+        private async void Button_上_Click(object sender, EventArgs e)
+        {
+            if (currentIndex <= 0) return;
+
+            ToggleButtons(false);
+
+            Debug.WriteLine("当前id{0}|访问id{1}", currentIndex, currentIndex - 1);
+
+            if (await LoadImageAsync(currentIndex - 1))
+                currentIndex--;
+
+            UpdateButtons();
+        }
+
+        private async void Button_下_Click(object sender, EventArgs e)
+        {
+            ToggleButtons(false);
+
+            Debug.WriteLine("当前id{0}|访问id{1}|{2}",
+                currentIndex, currentIndex + 1, currentIndex >= img.ImageCount - 2);
+
+            // 快看完时预加载
+            if (currentIndex >= img.ImageCount - 2)
+                _ = img.GetNextSetAsync(); // 后台加载，不阻塞 UI
+
+            if (await LoadImageAsync(currentIndex + 1))
+                currentIndex++;
+
+            UpdateButtons();
+        }
+
+        private async Task<bool> LoadImageAsync(int index)
+        {
+            var image = await img.GetImageAsync(index);
+            if (image == null) return false;
+
+            pictureBox.Image?.Dispose();
+            pictureBox.Image = image;
+
+            return true;
+        }
+
+        private void Button_存_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null) return;
+
+            string timestamp = DateTime.Now.ToString("yyyy-M-d_H-m-s");
+            string filePath = Path.Combine(SaveDirectory, $"{timestamp}.png");
+
+            pictureBox.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void Button_看_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", SaveDirectory);
+        }
+
+
+        //更改按钮状态
+        private void ToggleButtons(bool enabled)
+        {
+            button_上.Enabled = enabled;
+            button_下.Enabled = enabled;
+        }
+
+        private void UpdateButtons()
+        {
+            button_上.Enabled = currentIndex > 0;
+            button_下.Enabled = true;
+        }
+    }
+}
